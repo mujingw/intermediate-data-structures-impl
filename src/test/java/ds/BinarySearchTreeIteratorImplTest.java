47aad4cba7f5cef0b800d995def5b69c612cb3ae
mujingw <mqw5130@gmail.com>
2020-09-06 01:03:28 -0400

    Add test case to BinarySearchTreeIteratorImplTest.

--- src/test/java/ds/BinarySearchTreeIteratorImplTest.java
+++ src/test/java/ds/BinarySearchTreeIteratorImplTest.java
@@ -9,12 +9,13 @@ import org.junit.Test;
 
 public class BinarySearchTreeIteratorImplTest {
 
+    private BinaryTreeSerializerImpl<Integer> serializer;
     private BinarySearchTreeIteratorImpl<Integer> iterator;
     private BinaryTreeNode<Integer> root;
 
     @Before
     public void setUp() {
-        BinaryTreeSerializerImpl<Integer> serializer = new BinaryTreeSerializerImpl<>(Integer::valueOf);
+        serializer = new BinaryTreeSerializerImpl<>(Integer::valueOf);
         root = serializer.deserialize("2,1,X,X,3,X,X");
     }
 
@@ -35,6 +36,22 @@ public class BinarySearchTreeIteratorImplTest {
         Assert.assertEquals(Integer.valueOf(2), iterator.next());
         Assert.assertTrue(iterator.hasNext());
         Assert.assertEquals(Integer.valueOf(3), iterator.next());
+        Assert.assertFalse(iterator.hasNext());
+    }
+
+    @Test
+    public void nextTest2() {
+        root = serializer.deserialize("7,3,X,X,15,9,X,X,20,X,X");
+        iterator = new BinarySearchTreeIteratorImpl<>(root);
+
+        Assert.assertEquals(Integer.valueOf(3), iterator.next());
+        Assert.assertEquals(Integer.valueOf(7), iterator.next());
+        Assert.assertTrue(iterator.hasNext());
+        Assert.assertEquals(Integer.valueOf(9), iterator.next());
+        Assert.assertTrue(iterator.hasNext());
+        Assert.assertEquals(Integer.valueOf(15), iterator.next());
+        Assert.assertTrue(iterator.hasNext());
+        Assert.assertEquals(Integer.valueOf(20), iterator.next());
         Assert.assertFalse(iterator.hasNext());
     }
 
